6d2dacdfd9fc70f185973466fcfe8671d8b663d4
Matthew Tancik <tancik@mit.edu>
2015-11-14 15:24:44 -0500

    tested i2c multiple peripheral functionality

--- master_slave_i2c/slaveWire.ino/slaveWire.ino.ino
+++ master_slave_i2c/slaveWire.ino/slaveWire.ino.ino
@@ -14,8 +14,7 @@
 
 void setup() {
   Wire.begin(1);                // join i2c bus with address #8
-  Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  Wire.onRequest(requestEvent); // register event
 }
 
 void loop() {
@@ -24,11 +23,6 @@ void loop() {
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
-void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
-  }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
+void requestEvent() {
+  Wire.write("hello ");
 }
