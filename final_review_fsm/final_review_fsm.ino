3152dbf9aaa0dceb5d3c31d2dad40a1881000720
Zach Mikaya <zmikaya@gmail.com>
2015-12-06 02:45:08 -0500

    added elapsedMillis (time) lib and filled out many of the empty sate functions

--- final_review_fsm/final_review_fsm.ino
+++ final_review_fsm/final_review_fsm.ino
@@ -1,11 +1,12 @@
+#include <elapsedMillis.h>
+#include <FiniteStateMachine.h>
+
 //Implementation of the final review finite state machine (fsm)
 //code. It should be compatible with both the PCB and backup boards.
 //The full feature set to be implemented is yet to be determined.
 
 //Nothing has been tested.
 
-//libraries
-#include <FiniteStateMachine.h>
 
 //pin constants
 //interface switches/buttons
@@ -20,8 +21,11 @@ const int motorDirectionPin = 9;
 const int statusLEDPin = 10;
 
 //important global parameters
-const int motorSpeed = 50;
+const int forwardMotorSpeed = 50;
+const int reverseMotorSpeed = 128;
+const bool forwardDirection = HIGH;
 const int waitTime = 4000;
+elapsedMillis timeElapsed;
 
 //flags and global states
 //interrupts
@@ -33,7 +37,19 @@ bool statusPressed = false;
 const byte NUMBER_OF_STATES = 6;
 
 void doTurnedOn() {
-  return;
+  // This function was placed abote the initialize states to prevent a function 
+  // not defined. Furthremore, we must "extend" the function to prevent calling
+  // an FSM object function which does not exist yet.
+  if (checkHome()){
+    // if home, then wait
+    doTurnedOnTransitions(false);
+//    stateMachine.transitionTo(waiting); replaced above
+  }
+  else {
+    // if not home, go home
+    doTurnedOnTransitions(true);
+//    stateMachine.transitionTo(goingHome); replaced above
+  }
 }
 
 //initialize states
@@ -46,6 +62,15 @@ State poweredOff = State(doPoweredOff);
 
 FSM stateMachine = FSM(turnedOn);     //initialize state machine, start in state: On
 
+void doTurnedOnTransitions(bool nextState) {
+  if (nextState == true) {
+    stateMachine.transitionTo(goingHome);
+  }
+  else {
+    stateMachine.transitionTo(waiting);
+  }
+}
+
 void setup() {
   //this will need to be adjusted later
   Serial.begin(9600);
@@ -53,12 +78,15 @@ void setup() {
 //  pinMode(motorInterruptPin, OUTPUT);
 //  pinMode(switchPollingPin, INPUT);
 //  pinMode(motorDirectionPin, OUTPUT);
-//  attachInterrupt(digitalPinToInterrupt(motorInterruptPin), reverseMotor, RISING);
+  attachInterrupt(digitalPinToInterrupt(motorClosedPin), setMotorClosed, RISING);
+//  attachInterrupt(digitalPinToInterrupt(motorClosedPin), setMotorClosed, CHANGE);
 }
 
 void loop() {
   //perform main loop checks
+  
   checkPowerPressed();
+  
   checkStatusPressed();
   
   stateMachine.update();
@@ -87,21 +115,47 @@ void checkStatusPressed() {
   }
 }
 
-//state machine utility functions
-//void doTurnedOn() {
-//  return;
-//}
+bool checkHome() {
+  if (digitalRead(motorOpenPin) == HIGH) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+void setMotorClosed() {
+  motorClosed = true;
+}
+
+//state machine utility functions
 void doGoingHome() {
-  
+  if (checkHome()) {
+    analogWrite(motorPWMPin, 0);
+    timeElapsed = 0;
+    stateMachine.transitionTo(waiting);
+  }
+  else {
+    digitalWrite(motorDirectionPin, !forwardDirection);
+    analogWrite(motorPWMPin, reverseMotorSpeed);
+  }
 }
 
 void doWaiting() {
-  
+  if (timeElapsed >= waitTime) {
+    stateMachine.transitionTo(goingForward);
+  }
 }
 
 void doGoingForward() {
-  
+  if (motorClosed) {
+    analogWrite(motorPWMPin, 0);
+    stateMachine.transitionTo(stayingClosed);
+  }
+  else {
+    digitalWrite(motorDirectionPin, forwardDirection);
+    analogWrite(motorPWMPin, forwardMotorSpeed);
+  }
 }
 
 void doStayingClosed() {
